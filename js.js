7dfa27ef04f04540393c248aebb4b4dbd915f10b
YouROCK <113207809+yourock1988@users.noreply.github.com>
2024-08-13 23:50:56 +0300

    remove bad lines

--- js.js
+++ js.js
@@ -1,9 +1,2 @@
-let parsed = JSON.parse(null)
-
-parsed
-
-console.log('[3]' ?? '[]')
-console.log('[]' ?? '[]')
-console.log(null ?? '[]')
 console.log(undefined ?? '[]')
 console.log(false ?? '[]')
